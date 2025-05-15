443d4b24eeb78bd9a1fb859dfdd51db7865cbc28
Maryam Awadallah <140091330+Maryamawadallah@users.noreply.github.com>
2025-05-15 11:05:20 +0300

    Delete patient.cpp

--- patient.cpp
+++
@@ -1,8 +0,0 @@
-#include "patient.h"
-
-Patient::Patient(const std::string& name, const std::string& id, const std::string& history)
-    : name(name), id(id), history(history) {}
-
-std::string Patient::getName() const { return name; }
-std::string Patient::getId() const { return id; }
-std::string Patient::getHistory() const { return history; }
